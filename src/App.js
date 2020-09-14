5af4a73a0826b0a57548dcf00b18876a86269c6a
Eduardo Olguin <eddo555@hotmail.com>
2020-09-14 11:11:11 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Rock from "./img/rock.png";
+import Paper from "./img/paper.png";
+import Scissor from "./img/scissor.png";
 
 function App() {
+  const [myHand, setMyHand] = useState();
+  const [cpuHand, setCpuHand] = useState();
+  const [count, setCount] = useState(0);
+  const [cpuCount, setCpuCount] = useState(0);
+
+  function pickWinner(input) {
+    
+    const cpuInput = cpuHandSelect();
+    setMyHand(input);
+    setCpuHand(cpuInput)
+  }
+
+  function cpuHandSelect() {
+    var choice = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+
+    if (choice === 1) {
+      return 'rock'
+    } else if (choice === 2) {
+      return 'paper'
+    } else {
+      return 'scissor'
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{ backgroundColor: "#80FF72", width: "100vw", height: "100vh" }}
+    >
+      <h1>Choose</h1>
+      <img src={Rock} alt="rock" onClick={() => pickWinner("rock")} />
+      <img src={Paper} alt="paper" onClick={() => pickWinner("paper")} />
+      <img src={Scissor} alt="scissor" onClick={() => pickWinner("scissor")} />
+      <h4>{count}</h4>
+      <h4>{cpuCount}</h4>
     </div>
   );
 }
