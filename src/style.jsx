31acd49eec16f5a39ab37b7681150a3741ca51b7
Eduardo Olguin <eddo555@hotmail.com>
2020-09-14 16:37:17 +0200

    css done

--- src/style.jsx
+++ src/style.jsx
@@ -9,11 +9,12 @@ height:100vh;
 
 display:grid;
 grid-template-columns: repeat(10, 10%);
-grid-template-rows: repeat(5, 20%)
+grid-template-rows: repeat(5, 20%);
+font-family:"rockwell";
+
 }
 
 .title-wrapper {
-    border:solid green;
     grid-column:4/8;
     grid-row:1/3;
     display:flex;
@@ -23,23 +24,25 @@ grid-template-rows: repeat(5, 20%)
     font-size:6vw;
 }
 .hand-wrapper {
-    border:solid red;
     grid-column:2/10;
-    grid-row: 3/4;
+    grid-row: 3/5;
     display:flex;
     justify-content:space-between;
 }
 .hand {
      
      object-fit:contain;
-     max-width:300px;
+     max-width:200px;
      min-width:50px;
      
 }
 .counter-wrapper {
-    border:solid blue;
     grid-column: 2/10;
     grid-row: 5/5;
+    display:flex;
+    justify-content:space-around;
+    align-items:center;
+    font-size:4vw;
 }
 
 `
